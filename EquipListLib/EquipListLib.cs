d62c31c811ac0c8eafc9f3ce9d5981d2ce023d18
acii1ddd <vitiazagortsev@gmail.com>
2025-09-08 17:04:43 +0300

    initial

---
+++ EquipListLib/EquipListLib.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.InteropServices;
+
+namespace EquipListLib;
+
+public static class EquipListLib
+{
+    [UnmanagedCallersOnly(EntryPoint = "add")]
+    public static int Add(int a, int b) =>  a + b;
+}
